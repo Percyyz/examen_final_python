ef0b192dcd77e12172fcf52a3e0ac4e75757648c
percyz30 <percynoeyarihuaman@gmail.com>
2024-07-17 10:00:06 -0500

    guardo el ejercicio_cinco resuelto

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,15 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+
+def datos_alumno(nombre,apellido, edad, programa_estudio, semestre):
+    datos_alumno ={
+        "nombre": nombre,
+        "apellido":apellido,
+        "edad": edad,
+        "programa_estudio": programa_estudio,
+        "semestre": semestre
+    }
+    return datos_alumno
+datos = datos_alumno("percy","yarihuaman", 20, "APSTI", "III")
+print(datos)
