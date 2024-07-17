dd545cec6a5eb479f7e23ba712ad99d8a8422931
percyz30 <percynoeyarihuaman@gmail.com>
2024-07-17 09:22:29 -0500

    guardo el ejercicio_uno resuelto

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,18 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+
+def min_max(l):
+    minimo=l[0]
+    for n in l:
+        if n < minimo:
+            minimo=n
+
+    maximo=l[0]
+    for n in l:
+        if n > maximo:
+            maximo=n
+    return {"minimo":minimo, "maximo":maximo}
+lista_numeros=[1,3,54,6,5,3]
+resultado=min_max(lista_numeros)
+print (resultado)
