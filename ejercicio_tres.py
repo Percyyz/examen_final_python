7fdea09ccaeedc90f1727cdd484511cd6a897c62
percyz30 <percynoeyarihuaman@gmail.com>
2024-07-17 10:58:16 -0500

    guardo el ejercicio_tres resuelto

--- ejercicio_tres.py
+++ ejercicio_tres.py
@@ -4,3 +4,14 @@ ejm:
 entrada: eucalipto
 salida: {e:1,u:1,a:1,i:1,o:1}
 """
+def contar_vocales(texto):
+    vocales = 'aeiou'
+    conteo_vocales = {vocal: 0 for vocal in vocales}
+    for letra in texto:
+        if letra in vocales:
+            conteo_vocales[letra] += 1
+    resultado = {vocal: conteo_vocales[vocal] for vocal in vocales if conteo_vocales[vocal] > 0}
+    return resultado
+texto = "eucalipto"
+resultado = contar_vocales(texto)
+print(resultado)
